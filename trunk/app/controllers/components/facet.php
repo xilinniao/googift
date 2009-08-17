0088c60d67ff5cd21c25f804675f8239c9d1b1a9
hfdang <hfdang@ebdc26c0-8653-11de-bdfc-c94c2ab25652>
2009-08-17 09:52:01 +0000

    Implement search functions.

--- trunk/app/controllers/components/facet.php
+++ trunk/app/controllers/components/facet.php
@@ -1,50 +1,82 @@
 <?php
 class FacetComponent extends Object {
 
-	/**
-	 * @param $plusLine e.g. day{Birthday/Christmas},age{<40}
-	 * @param $keywords The search input. e.g. 生日 妻子 29岁
-	 */
-	public function computePlus($plusLine, $keywords) {
-		$facetPlus_array = explode(',', $plusLine);
-		$totalPlus = 0;
-		foreach ($facetPlus_array as $facetPlus) {
-			$index = strpos($facetPlus, "{");
-			$facetName = substr($facetPlus, 0, $index);
-			$cates = substr($facetPlus, $index +1, strlen($facetPlus) - $index -2);
+    var $components = array('VectorGenerator');
+    /**
+     * @param $plusLine e.g. day{Birthday/Christmas},age{<40}
+     * @param $keywords The search input. e.g. 生日 妻子 29岁
+     */
+    public function computePlus($plusLine, $keywords) {
+        $facetPlus_array = explode(',', $plusLine);
+        $totalPlus = 0;
+        foreach ($facetPlus_array as $facetPlus) {
+            $index = strpos($facetPlus, "{");
+            $facetName = substr($facetPlus, 0, $index);
+            $cates = substr($facetPlus, $index +1, strlen($facetPlus) - $index -2);
 
-			require_once ("facet/" . $facetName . ".php");
-			$class = new ReflectionClass($facetName);
-			if ($class) {
-				$method = $class->getMethod("getKeywordPlus");
-				$plus = $method->invoke($class->newInstance(), $cates, $keywords);
-				$totalPlus += $plus;
-			}
-		}
-		return $totalPlus / sizeof($facetPlus_array);
-	}
+            require_once ("facet/" . $facetName . ".php");
+            $class = new ReflectionClass($facetName);
+            if ($class) {
+                $method = $class->getMethod("getKeywordPlus");
+                $plus = $method->invoke($class->newInstance(), $cates, $keywords);
+                $totalPlus += $plus;
+            }
+        }
+        return $totalPlus / sizeof($facetPlus_array);
+    }
 
-	/**
-	 * Given a facet plus values and a keywords input, compute the plus value for this facet to add to the keywords.
-	 * @param $plusKeys The plus values of this facet. e.g. 生日,情人节 或者 30~40.
-	 * @param $keywords The keywords user input. e.g. 生日 妻子 29岁
-	 */
-	//	static abstract function getKeywordPlus($plusKeys, $keywords);
+    /**
+     * Given a facet plus values and a keywords input, compute the plus value for this facet to add to the keywords.
+     * @param $plusKeys The plus values of this facet. e.g. 生日,情人节 或者 30~40.
+     * @param $keywords The keywords user input. e.g. 生日 妻子 29岁
+     */
+    //	static abstract function getKeywordPlus($plusKeys, $keywords);
 
-	function getFacetArray() {
-		$inputString = '女的  50岁 生日 妗子';
-		$vector = array();
-		require_once ("facet/facetbase.php");
-		$facetBase = facetbase::getFacetArray();
-		foreach ($facetBase as $facetName => $facetArray) {
-			require_once ("facet".DS.$facetName.".php");
-			$class = new ReflectionClass($facetName);
-			if ($class) {
-				$method = $class->getMethod("process");
-				$vector = $method->invoke($class->newInstance(), $inputString, $vector);
-			}
-		}
-		return $vector;
-	}
+    function getFacetArray() {
+        $inputString = '女的  50岁 生日 妗子';
+        $array = array();
+        require_once ("facet/facetbase.php");
+        $facetBase = facetbase::getFacetArray();
+        foreach ($facetBase as $facetName => $facetArray) {
+            require_once ("facet".DS.$facetName.".php");
+            $class = new ReflectionClass($facetName);
+            if ($class) {
+                $method = $class->getMethod("process");
+                $array = $method->invoke($class->newInstance(), $inputString, $array);
+            }
+        }
+
+        $vector = array();
+        foreach ($facetBase as $facetName => $facetArray) {
+            $class = new ReflectionClass($facetName);
+            if ($class) {
+                $method = $class->getMethod("toVector");
+                $vector = $method->invoke($class->newInstance(), $array, $vector);
+            }
+        }
+
+
+//        $array = array(
+//            'day' => '情人节|生日',
+//            'acceptor'=>'父辈|母亲',
+//            'age'=>'<10|35~45',
+//            'gender'=>'男|女'
+//            );
+//            $vector = array();
+//            foreach ($facetBase as $facetName => $facetArray) {
+//                $class = new ReflectionClass($facetName);
+//                if ($class) {
+//                    $method = $class->getMethod("toVector");
+//                    $vector = $method->invoke($class->newInstance(), $array, $vector);
+//                }
+//            }
+//            return $vector;
+
+    $string = 'day{生日|情人节},age{<20|45~24}';
+    $a = $this->VectorGenerator->deserialize($string);
+    $s = $this->VectorGenerator->serialize($a);
+    $a['original'] = $s;
+    return $a;
+    }
 }
 ?>
--- trunk/app/controllers/components/facet/age.php
+++ trunk/app/controllers/components/facet/age.php
@@ -5,6 +5,34 @@ class age extends facetbase {
 	function getName() {
 		return 'age';
 	}
+	
+	function getPrefixIndex() {
+		return '1';
+	}
+	
+	function getRangeSplits() {
+		$ranges = array(
+		    '0',
+			'1',
+			'3',
+			'5',
+			'10',
+			'15',
+			'20',
+			'25',
+			'30',
+			'35',
+			'40',
+			'45',
+			'50',
+			'60',
+			'70',
+			'80',
+		    '100',
+		    ContinuousValueUtil::$POS_INFINITE
+		);
+		return $ranges;
+	}
 
 	function getMatchPatterns() {
 		return array (
