5f7b42deacc5eb6f1ff119cd434851d0f7deee57
hfdang <hfdang@ebdc26c0-8653-11de-bdfc-c94c2ab25652>
2009-12-07 07:28:11 +0000

    remove unused.

---
+++ WebCrawler/src/cn/googift/recognition/analysis/PositionInPlate.java
@@ -0,0 +1,10 @@
+package cn.googift.recognition.imageanalysis;
+
+public class PositionInPlate {
+    public int x1;
+    public int x2;
+    PositionInPlate(int x1,int x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+}
