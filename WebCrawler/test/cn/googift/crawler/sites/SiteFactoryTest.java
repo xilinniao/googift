1748b9aea10d749ddeaefbce269d3e8e198245ee
wldandan <wldandan@ebdc26c0-8653-11de-bdfc-c94c2ab25652>
2010-01-12 08:34:40 +0000

    change constructor from protected to public

--- WebCrawler/test/cn/googift/crawler/sites/SiteFactoryTest.java
+++ WebCrawler/test/cn/googift/crawler/sites/SiteFactoryTest.java
@@ -1,14 +1,35 @@
 package cn.googift.crawler.sites;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import cn.googift.crawler.sites.jingdong.JDSite;
+import cn.googift.crawler.sites.newegg.NewEggSite;
 import junit.framework.TestCase;
 
 public class SiteFactoryTest extends TestCase {
-    public void testGetSiteIterator() {
-        final SiteIterator siteIterator = SiteFactory.getSiteIterator();
-        assertTrue(siteIterator.hasNext());
-        final Site site = siteIterator.next();
-        assertNotNull(site);
-        assertTrue(site instanceof JDSite);
-    }
+   Map<String, Site> sites;
+   Site JDSite;
+   Site NewEggSite;
+   
+   public void setUp()
+   {
+	   JDSite = new JDSite();
+	   NewEggSite = new NewEggSite();
+	   sites = new HashMap<String, Site>();
+	   sites.put(JDSite.getDomain(), JDSite);
+	   sites.put(NewEggSite.getDomain(), NewEggSite);
+	   
+   }
+   
+   public void testCrawlSites()
+   {
+	   SiteFactory.getInstance().setSites(sites);
+	   SiteFactory.getInstance().crawlSites();
+	   while (true)
+	   {
+	   }
+	   
+   }
+	
 }
